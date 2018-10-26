9f79cfa64c601c65ceed3ba295772b5fd9a3c8b9
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-10-26 20:29:33 +0200

    add unit test for Event model

--- events/tests.py
+++ events/tests.py
@@ -42,4 +42,22 @@ class CreateEventTest(TestCase):
 
 
 class EventModelTest(TestCase):
-    pass
+    def test_create_read_event(self):
+        # create events
+        event_one = Event()
+        event_one.title = 'Test event #1'
+        event_one.save()
+
+        event_two = Event()
+        event_two.title = 'Test event #2'
+        event_two.save()
+
+        # read events
+        events = Event.objects.all()
+        self.assertEqual(events.count(), 2)
+
+        saved_event_one = events[0]
+        saved_event_two = events[1]
+
+        self.assertIn('Test event #1', saved_event_one.title)
+        self.assertIn('Test event #2', saved_event_two.title)
