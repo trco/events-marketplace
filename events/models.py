9db5a94e9d7be2720ae64f5f72a475b0d848eb81
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-11-17 20:44:29 +0100

    change title field to CharField

--- events/models.py
+++ events/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.TextField(max_length=128)
+    title = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
